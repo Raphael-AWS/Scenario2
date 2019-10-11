265e41ed971880019fa5362d13bef0d6e7807751
Raphael-AWS <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-11 15:53:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "aws_key_path" {
 }
 variable "aws_key_name" {
   description = "Key name for SSHing into EC2"
-  default = "Your key name"
+  default = "blockchain"
 }
 variable "aws_availability_zones" {
   default     = "us-east-1a,us-east-1b,us-east-1c,us-east-1d"
