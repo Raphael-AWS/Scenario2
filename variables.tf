8d662f034fe1e9c66bfeac54cf2c17dee690c5ea
Raphael-AWS <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-11 15:49:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,14 +8,6 @@ variable "aws_region" {
   description = "AWS region for hosting our your network"
   default = "us-east-1"
 }
-variable "access_key"{
-description = "AWS Access Key"
-default = "Your Access Key"
-}
-variable "secret_key"{
-description = "AWS Secret Key"
-default = "Your Secret Key"
-}
 variable "amis" {
     description = "AMIs by region"
     default = {
