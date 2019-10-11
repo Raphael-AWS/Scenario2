f5e882a96dd47145d3136f10902aa02775ae33e7
Raphael-AWS <56334419+Raphael-AWS@users.noreply.github.com>
2019-10-11 15:49:21 +0530

    Update project.tf

--- project.tf
+++ project.tf
@@ -1,3 +1,11 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-jenkins-s3"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 #setup Provider
 provider "aws" {
   region = "${var.aws_region}"
